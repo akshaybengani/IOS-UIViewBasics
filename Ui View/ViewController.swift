3c638870070cd07b937087bed150f2af1edce0a3
Akshay Bengani <akshay@rirev.com>
2020-11-27 15:12:43 +0530

    Project Added

---
+++ Ui View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ui View
+//
+//  Created by malav desai on 23/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
